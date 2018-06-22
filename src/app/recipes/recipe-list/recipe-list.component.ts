b51a318f33c34493c4f66423a5dddaedec499525
Janhavi Parulekar <parulekar.j@husky.neu.edu>
2018-06-22 00:56:16 -0400

    Navigation complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,12 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [new Recipe("Test Recipe", "This is a test recipe", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"),
-  new Recipe("Test Recipe", "This is a test recipe", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")];
+  new Recipe("Another Test Recipe", "This is a test recipe", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  // nameCreated =
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
   }
 
 }
